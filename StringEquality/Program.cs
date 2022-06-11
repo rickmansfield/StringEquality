f1c7daff9f533a5b8e85cd52e2edaf2d32526cf3
Rick Mansfield <PEACEFULRICK@msn.com>
2022-06-11 12:43:31 -0500

    Section 9 Video 131 String Equality

--- StringEquality/Program.cs
+++ StringEquality/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace StringEquality
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            //string first = "This is a string";
+            //string second = "This is a string";
+            //Console.WriteLine(first == second);//test that the two objects are the same
+            //Console.WriteLine(second == first);//test that the two objects are the same
+            //Console.WriteLine(first.Equals(second));//test that object VALUES are the same
+            //Console.WriteLine(second.Equals(first));//test that object VALUES are the same
+
+            //------------Section 9 video 131 String Equality---------------------
+            string first = "This is a string";
+            string second = "THIS IS A STRING";
+            Console.WriteLine(first == second);//test that the two objects are the same
+            Console.WriteLine(second == first);//test that the two objects are the same
+            Console.WriteLine(first.Equals(second, StringComparison.OrdinalIgnoreCase));//test that object VALUES are the same
+            Console.WriteLine(second.Equals(first, StringComparison.OrdinalIgnoreCase));//test that object VALUES are the same
         }
     }
 }
